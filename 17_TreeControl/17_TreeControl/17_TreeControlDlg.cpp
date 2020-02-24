08f36e5d83aec098350f7f68b04baec9510aff74
yangjian <952801974@qq.com>
2020-02-24 17:29:01 +0800

    TreeControl控件

---
+++ 17_TreeControl/17_TreeControl/17_TreeControlDlg.cpp
@@ -0,0 +1,180 @@
+﻿
+// 17_TreeControlDlg.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "17_TreeControl.h"
+#include "17_TreeControlDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMy17TreeControlDlg 对话框
+
+
+
+CMy17TreeControlDlg::CMy17TreeControlDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MY17_TREECONTROL_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMy17TreeControlDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_TREE1, m_treeCtrl);
+}
+
+BEGIN_MESSAGE_MAP(CMy17TreeControlDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CMy17TreeControlDlg 消息处理程序
+
+BOOL CMy17TreeControlDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	HICON icon[4];
+	icon[0] = AfxGetApp()->LoadIconW(IDI_ICON1);
+	icon[1] = AfxGetApp()->LoadIconW(IDI_ICON2);
+	icon[2] = AfxGetApp()->LoadIconW(IDI_ICON3);
+	icon[3] = AfxGetApp()->LoadIconW(IDI_ICON4);
+
+	m_imgList.Create(30, 30, ILC_COLOR32, 4, 4);
+	for (int x = 0; x < 4; x++)
+	{
+		m_imgList.Add(icon[x]);
+	}
+
+	m_treeCtrl.SetImageList(&m_imgList, TVSIL_NORMAL);
+
+	HTREEITEM root = m_treeCtrl.InsertItem(_T("根节点"),0,0,NULL);
+	HTREEITEM father = m_treeCtrl.InsertItem(_T("父节点"), 1, 1, root);
+	HTREEITEM son = m_treeCtrl.InsertItem(_T("子节点"), 2, 2, father);
+
+
+	HTREEITEM root1 = m_treeCtrl.InsertItem(_T("根节点"), 0, 0, NULL);
+	HTREEITEM father1 = m_treeCtrl.InsertItem(_T("父节点"), 1, 1, root1);
+	HTREEITEM son1 = m_treeCtrl.InsertItem(_T("子节点"), 2, 2, father1);
+
+	//设置某个节点被选中(展开)
+	m_treeCtrl.SelectItem(father);
+	m_treeCtrl.SelectItem(son1);
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CMy17TreeControlDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMy17TreeControlDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMy17TreeControlDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ 17_TreeControl/17_TreeControl/17_TreeControlDlg.h
@@ -0,0 +1,37 @@
+﻿
+// 17_TreeControlDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMy17TreeControlDlg 对话框
+class CMy17TreeControlDlg : public CDialogEx
+{
+// 构造
+public:
+	CMy17TreeControlDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY17_TREECONTROL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+private:
+	CImageList m_imgList;
+	CTreeCtrl m_treeCtrl;
+};
